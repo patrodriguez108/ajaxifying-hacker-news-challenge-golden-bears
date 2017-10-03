3e23f1295f469b9f8c31de3f501f9d15ca03d889
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-10-03 09:18:56 -0500

    Add logic for upvote

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,7 +10,11 @@ end
 post '/posts/:id/vote' do
   post = Post.find(params[:id])
   post.votes.create(value: 1)
-  redirect "/posts"
+  if request.xhr?
+    erb :"/votes/_vote_points", locals: {post: post}, layout: false
+  else
+    redirect "/posts"
+  end
 end
 
 delete '/posts/:id' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#js-vote").on("submit", function() {
+    event.preventDefault();
+    $form = $(this);
+    var method = $form.attr("method");
+    var url = $form.attr("action");
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $.ajax({
+      method: method,
+      url: url
+    }).done(function(response) {
+      $("#js-post-details").children(".points").replaceWith(response);
+      $form.children("#js-upvote-button").css("color", "red")
+    })
+  })
+  // $(".upvote-button").on("click", function() {
+  //   event.preventDefault();
+  //   var $button = $(this);
+
+  //   $button.css("color", "red");
+  // })
 });
